3cab4081c2f57dda86524fd6523ecd35c5f5a75a
Tanushree Arora <aroratanushree.28@gmail.com>
2024-02-20 16:47:22 -0500

    Initial Commit

---
+++ Icebreaker_Arora_S24/Icebreaker_Arora_S24/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Icebreaker_Arora_S24
+//
+//  Created by Tanushree Arora on 2/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
